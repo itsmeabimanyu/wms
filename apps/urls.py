168892fd1a05850ca6bd79dca22f07384a01554c
itsmeabimanyu <abimanyubima@hotmail.com>
2025-10-20 16:26:31 +0700

    add global static folder with JS and CSS setup

---
+++ apps/urls.py
@@ -0,0 +1,10 @@
+from django.shortcuts import redirect
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', lambda request: redirect('dashboard')),
+    path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
+]
