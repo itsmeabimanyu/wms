cd92ab6e08e860e55ae7e6fb67d6537d860c2086
itsmeabimanyu <abimanyubima@hotmail.com>
2025-10-21 06:12:53 +0700

    split base layout into partial includes

--- apps/urls.py
+++ apps/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', lambda request: redirect('dashboard')),
     path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
+    path('warehouse/', views.Warehouse.as_view(), name='warehouse'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -6,3 +6,58 @@ from django.views.generic import (
 # Create your views here.
 class Dashboard(TemplateView):
     template_name = 'layouts/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        menu_sections = [
+            {
+                "label": "Elements",
+                "items": [
+                    {"name": "Buttons", "url": "elem-buttons.html"},
+                    {"name": "Dropdown", "url": "elem-dropdown.html"},
+                    {"name": "Icons", "url": "elem-icons.html"},
+                ],
+            },
+            {
+                "label": "Forms",
+                "items": [
+                    {"name": "Form Elements", "url": "form-elements.html"},
+                ],
+            },
+            {
+                "label": "Charts",
+                "items": [
+                    {"name": "ChartJS", "url": "chart-chartjs.html"},
+                ],
+            },
+            {
+                "label": "Tables",
+                "items": [
+                    {"name": "Basic Tables", "url": "table-basic.html"},
+                ],
+            },
+        ]
+
+        context['menu_sections'] = menu_sections
+
+        return context
+
+class Warehouse(TemplateView):
+    template_name = 'pages/summary.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        menu_sections = [
+            {
+                "label": "Warehouse",
+                "items": [
+                    {"name": "Warehouse Location", "url": "elem-icons.html"},
+                    {"name": "Storage Location", "url": "elem-buttons.html"},
+                    {"name": "Bin Location", "url": "elem-dropdown.html"},
+                ],
+            },
+        ]
+
+        context['menu_sections'] = menu_sections
+
+        return context
